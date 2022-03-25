c9dd23295e17e6a8db348cc659ce7e15bb99179a
marvin obig <obig.marv@gmail.com>
2022-03-25 12:48:31 +0000

    add event listener for search button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./index.scss";
+import { getLocationCoordinates } from "./modules/locationCoordinates";
+import { getWeatherData } from "./modules/weatherData";
+
+const searchBtn = document.getElementById("submit");
+searchBtn.addEventListener("click", app);
+
+
+function app() {
+    getLocationCoordinates();
+    getWeatherData();
+}
