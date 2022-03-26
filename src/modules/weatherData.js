4d0cea09df3b053b7275b2c23b576b4ee8654a9b
marvin obig <obig.marv@gmail.com>
2022-03-26 12:15:39 +0000

    paste weatherData async function

--- src/modules/weatherData.js
+++ src/modules/weatherData.js
@@ -1,4 +1,24 @@
-import { lat, lon } from "./locationCoordinates";
+let lat = 0;
+let lon = 0;
+
+async function getLocationCoordinates() {
+  const location = document.getElementById("search");
+
+  const coordinates = await fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      location.value +
+      "&limit=1&appid=56ee2236b5d006c6606bcbf8167154c3",
+    { mode: "cors" }
+  );
+
+  const result = await coordinates.json();
+
+  lat = result[0].lat;
+  lon = result[0].lon;
+  console.log(result, lat, lon);
+
+  getWeatherData();
+}
 
 async function getWeatherData() {
   const weather = await fetch(
@@ -13,4 +33,4 @@ async function getWeatherData() {
   console.log(result);
 }
 
-export { getWeatherData };
+export { getLocationCoordinates };
