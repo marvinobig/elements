1d5ed504f3741d198ea73661af2cbcc6ba892335
marvin obig <obig.marv@gmail.com>
2022-03-26 16:58:49 +0000

    add card generator function for daily forecast

--- src/modules/weatherDisplay.js
+++ src/modules/weatherDisplay.js
@@ -17,14 +17,48 @@ function currentWeatherDisplay(data) {
   description.textContent = `${data.current.weather[0].description}`;
   feelsLike.textContent = `${data.current.feels_like}°`;
   humidity.textContent = `${data.current.humidity}%`;
-  windSpeed.textContent = `${data.current.wind_speed} m/s`
+  windSpeed.textContent = `${data.current.wind_speed} m/s`;
 }
 
-
 function dailyWeatherDisplay(data) {
-    const dailyCardContainer = document.getElementById("daily-forecast")
+  removePreviousDailyForecast();
+
+  for (let i = 1; i < data.daily.length; i++) {
+    const dailyCardContainer = document.getElementById("daily-forecast");
+
     const dailyCard = document.createElement("div");
+    const dailyDay = document.createElement("p");
     const dailyIcon = document.createElement("img");
+    const temp = document.createElement("p");
+    const description = document.createElement("p");
+
+    dailyCard.classList.add("daily-card");
+    dailyDay.classList.add("daily-day");
+    dailyIcon.classList.add("daily-icon");
+    temp.classList.add("daily-temp");
+    description.classList.add("daily-description");
+
+    dailyDay.textContent = i;
+    dailyIcon.src = `http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`;
+    temp.textContent = `${data.daily[i].temp.min}° / ${data.daily[i].temp.max}°`;
+    description.textContent = `${data.daily[i].weather[0].description}`;
+
+    dailyCardContainer.appendChild(dailyCard);
+    dailyCard.appendChild(dailyDay);
+    dailyCard.appendChild(dailyIcon);
+    dailyCard.appendChild(temp);
+    dailyCard.appendChild(description);
+  }
+}
+
+function removePreviousDailyForecast() {
+  const previousDailyCards = document.querySelectorAll(".daily-card");
+
+  previousDailyCards.forEach((element) => {
+    if (element !== undefined) {
+      element.remove();
+    }
+  });
 }
 
 export { locationDisplay, currentWeatherDisplay, dailyWeatherDisplay };
