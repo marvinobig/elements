a324f5b2e7417b2711a8195b6e0fdd627bcab2b5
marvin obig <obig.marv@gmail.com>
2022-03-26 13:50:49 +0000

    display current weather data

--- src/modules/weatherDisplay.js
+++ src/modules/weatherDisplay.js
@@ -1,6 +1,23 @@
-import { getLocationCoordinates } from "./weatherData";
-
-
-function Location(data) {
-    const 
+function locationDisplay(data) {
+  const locationPara = document.getElementById("location");
+  locationPara.textContent = `${data.name} / ${data.state}`;
 }
+
+function currentWeatherDisplay(data) {
+  const icon = document.getElementById("current-weather-icon");
+  const temp = document.getElementById("temp");
+  const description = document.getElementById("current-weather-description");
+  const feelsLike = document.getElementById("feels-like");
+  const humidity = document.getElementById("humidity");
+  const windSpeed = document.getElementById("wind-speed");
+
+  icon.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+  icon.alt = "current weather icon";
+  temp.textContent = `${data.current.temp}°`;
+  description.textContent = `${data.current.weather[0].description}`;
+  feelsLike.textContent = `${data.current.feels_like}°`;
+  humidity.textContent = `${data.current.humidity}%`;
+  windSpeed.textContent = `${data.current.wind_speed} mph`
+}
+
+export { locationDisplay, currentWeatherDisplay };
