4cca38e3d0b9b795938257f596fd4cbada6a072c
marvin obig <obig.marv@gmail.com>
2022-03-27 11:40:29 +0100

    add function to detect enter key press

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import { getLocationCoordinates } from "./modules/weatherData";
 const searchBtn = document.getElementById("submit");
 searchBtn.addEventListener("click", app);
 
+const searchBar = document.getElementById("search");
+searchBar.addEventListener("keypress", e => {
+  if (e.key === "Enter") {
+    app();
+  }
+})
+
 function app() {
   getLocationCoordinates().catch((err) => {
     console.log(err);
